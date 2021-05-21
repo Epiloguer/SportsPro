01bc52b7f3d2f11706b8b78ef3f5668679fda481
irshaad-sait <Irshaad.Sardiwalla@edu.sait.ca>
2021-05-21 17:32:43 -0600

    Added AccountController, Added propeties to User.cs

--- SportsPro/Models/DomainModels/User.cs
+++ SportsPro/Models/DomainModels/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,10 @@ namespace SportsPro.Models
     public class User : IdentityUser 
     {
         //Inherits all IdentityUser properties
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        [NotMapped]
+        public IList<string> RoleNames { get; set; }
     }
 }
