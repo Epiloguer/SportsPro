2f5755b65253c925293c36bf63f550e2f5f198ea
Epiloguer <ssapergia99@gmail.com>
2021-05-22 09:50:31 -0600

    Created Admin area and User controller

---
+++ SportsPro/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using SportsPro.Models;
+
+namespace SportsPro.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserManager<User> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public UserController(UserManager<User> userMngr,
+            RoleManager<IdentityRole> roleMngr)
+        {
+            userManager = userMngr;
+            roleManager = roleMngr;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<User> users = new List<User>();
+            foreach (User user in userManager.Users) 
+            {
+                user.RoleNames = await userManager.GetRolesAsync(user);
+                users.Add(user);
+            }
+            UserViewModel model = new UserViewModel
+            {
+                Users = users,
+                Roles = roleManager.Roles
+            };
+            return View(model);
+        }
+    }
+}
