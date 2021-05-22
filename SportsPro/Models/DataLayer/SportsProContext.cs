99ba602d98742f5e51277cac7e0698c4402a229d
irshaad-sait <Irshaad.Sardiwalla@edu.sait.ca>
2021-05-22 17:17:27 -0600

    added seed data for custprod

--- SportsPro/Models/DataLayer/SportsProContext.cs
+++ SportsPro/Models/DataLayer/SportsProContext.cs
@@ -305,6 +305,30 @@ namespace SportsPro.Models
                     DateClosed = null
                 }
             );
+
+
+            modelBuilder.Entity<CustProd>().HasData(
+                   new CustProd { CustomerID = 1002, ProductID = 1 }, 
+                   new CustProd { CustomerID = 1004, ProductID = 2 }, 
+                   new CustProd { CustomerID = 1006, ProductID = 1 }, 
+                   new CustProd { CustomerID = 1008, ProductID = 3 }, 
+                   new CustProd { CustomerID = 1010, ProductID = 4 }, 
+                   new CustProd { CustomerID = 1012, ProductID = 5 }, 
+                   new CustProd { CustomerID = 1015, ProductID = 5 }, 
+                   new CustProd { CustomerID = 1002, ProductID = 2 }, 
+                   new CustProd { CustomerID = 1002, ProductID = 3 }, 
+                   new CustProd { CustomerID = 1004, ProductID = 3 }, 
+                   new CustProd { CustomerID = 1006, ProductID = 3 }, 
+                   new CustProd { CustomerID = 1008, ProductID = 4 }, 
+                   new CustProd { CustomerID = 1010, ProductID = 5 }, 
+                   new CustProd { CustomerID = 1012, ProductID = 3 }, 
+                   new CustProd
+                   {
+                       CustomerID = 1015,
+                       ProductID = 4
+                   }
+                );
+            
         }
         public static async Task CreateAdminUser(IServiceProvider serviceProvider)
         {
