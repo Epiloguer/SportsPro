4de9deb76aee2d2259c46150f6320fcff30892ca
Epiloguer <ssapergia99@gmail.com>
2021-05-22 18:26:56 -0600

    Started Registration List

--- SportsPro/Models/ViewModels/RegistrationViewModel.cs
+++ SportsPro/Models/ViewModels/RegistrationViewModel.cs
@@ -8,6 +8,7 @@ namespace SportsPro.Models
     public class RegistrationViewModel
     {
         public List<Customer> Customers { get; set; }
+        public List<Product> Products { get; set; }
         public Customer Customer { get; set; }
         public Product Product { get; set; }
 
