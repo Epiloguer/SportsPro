a9b73f853629b4d855426be3531b099a7fb358e1
Epiloguer <ssapergia99@gmail.com>
2021-05-25 15:31:07 -0600

    Implemented Testing

---
+++ SportsProTest/NavTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using SportsPro.Models;
+
+namespace SportsProTest
+{
+    public class NavTests
+    {
+        [Fact]
+        public void ActiveMethod_ReturnsAString()
+        {
+            string s1 = "Home";                 //arrange
+            string s2 = "Books";
+
+            var result = Nav.Active(s1, s2);    //act
+
+            Assert.IsType<string>(result);      //assert
+        }
+
+        [Theory]
+        [InlineData("Product", "Product")]
+        [InlineData("Customer", "Customer")]
+        public void ActiveMethod_ReturnsValueActiveIfMatch(string s1, string s2)
+        {
+            string expected = "active";     //arrange
+
+            var result = Nav.Active(s1, s2);    //act
+
+            Assert.Equal(expected, result);     //assert
+        }
+    }
+}
