c85bb1c0dbc25655dd329429563b40e8c112a6ea
Epiloguer <ssapergia99@gmail.com>
2021-05-25 14:17:41 -0600

    Commit

---
+++ SportsProTest/ProductControllerTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using SportsPro.Areas.Admin.Controllers;
+
+namespace SportsProTest
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
